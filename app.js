30258013fbd849221e8126f758cf70602e51d07f
alifaisal987 <alifaisal987@gmail.com>
2024-06-29 20:25:20 +0500

    java script practice

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log("hello javascript");
+var username = 'ali';
+console.log(username);
+
+// alert ('ali');
+
+console.log(2+2);
+
+var num1 = 2+2;
+var num2 =4+4;
+
+console.log(num1+num2);
+
+var city='karachi';  //declaration
+console.log(city);
+var city='lahore';   //redeclaration
+console.log(city);
+city ='islamabad';  //reassign
+console.log(city);
+
+
+
+weight =30;
+console.log(weight+25);

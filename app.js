41a7faab42ba5db43f644e02d754f4fb0e98f9df
alifaisal987 <alifaisal987@gmail.com>
2024-06-30 14:01:54 +0500

    notes added

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-console.log("hello javascript");
-var username = 'ali';
-console.log(username);
+// console.log("hello javascript");
+// var username = 'ali';
+// console.log(username);
 
-alert ('ali');
+// alert ('ali');
 
-console.log(2+2);
+// console.log(2+2);
 
-var num1 = 2+2;
-var num2 =4+4;
+// var num1 = 2+2;
+// var num2 =4+4;
 
-console.log(num1+num2);
+// console.log(num1+num2);
 
-var city='karachi';  //declaration
-console.log(city);
-var city='lahore';   //redeclaration
-console.log(city);
-city ='islamabad';  //reassign
-console.log(city);
+// var city='karachi';  //declaration
+// console.log(city);
+// var city='lahore';   //redeclaration
+// console.log(city);
+// city ='islamabad';  //reassign
+// console.log(city);
 
 
 
-weight =30;
-console.log(weight+25);
+// weight =30;
+// console.log(weight+25);
+
+// data types in javascript
+// string 
+// number 
+// boolean
